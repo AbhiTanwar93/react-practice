ab3f75e6a700d3a91827b3eb17835a9e77473120
AbhiTanwar93 <tanwarabhi1993@gmail.com>
2023-09-29 00:11:04 +0530

    changes for header footer using flex in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import MainContent from './main-content/MainContent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <MainContent />
+      <div className='App-footer'>
+        <span className='copyright'>&copy; {(new Date().getFullYear())}</span>
+      </div>
     </div>
   );
 }
---
+++ src/main-content/MainContent.js
@@ -0,0 +1,11 @@
+import './MainContent.css';
+
+function MainContent() {
+  return (
+    <div className="Main-content">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default MainContent;
